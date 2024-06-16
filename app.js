75ca6981d1a83dc367e12d801e55ca9bda999e60
Muhammad Daniyal <mdaniyal5454@gmail.com>
2024-06-16 13:03:36 +0500

    Updated index.html, style.css and app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let Url = "https://api.openweathermap.org/data/2.5/weather?";
-const ApiKey = "86960b2a6adfa182188e00404e6d2fce";
+const apiKey = "86960b2a6adfa182188e00404e6d2fce";
 
 const cityName = document.querySelector("#city-name");
-const searchButton = document.querySelector(".search-button");
+const searchButton = document.querySelector("#search-button");
 const temperature = document.querySelector("#temp");
 const sky = document.querySelector("#sky");
 const weatherLocation = document.querySelector("#location");
@@ -13,3 +13,10 @@ const sunrise = document.querySelector("#sunrise");
 const sunset = document.querySelector("#sunset");
 const humidity = document.querySelector("#humidity");
 const visibility = document.querySelector("#visibility");
+
+const fetchCurrentWeatherData = async() => {
+    const apiUrl = `${Url}q=${cityName.value}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+}
